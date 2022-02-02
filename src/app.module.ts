65ae9a1419ef36d8996eb60ac44e9124cc0b4c31
marinambur <marinaburykina@gmail.com>
2022-02-03 00:24:03 +0200

    Подключение базы

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Article } from './article.model';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      username: 'test',
+      password: 'test',
+      database: 'test',
+      logging: true,
+      synchronize: true,
+      entities: [Article],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/article.model.ts
+++ src/article.model.ts
@@ -1,11 +1,18 @@
-export class Article {
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Article extends BaseEntity {
+    @PrimaryGeneratedColumn()
     id: number;
 
+    @Column()
     title: string;
 
+    @Column()
     content: string;
 
-    constructor (title: string, content: string, id?: number) {
+    constructor(title: string, content: string, id?: number) {
+        super();
         this.id = id;
         this.title = title;
         this.content = content;
